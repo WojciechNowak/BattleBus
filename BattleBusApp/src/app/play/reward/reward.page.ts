213d80f587407fb87d8815c2763330fd03f4e15b
Wojciech Nowak <wojciech.nowak@trapezegroup.pl>
2022-04-07 22:11:46 +0200

    reward page

--- BattleBusApp/src/app/play/reward/reward.page.ts
+++ BattleBusApp/src/app/play/reward/reward.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-reward',
@@ -7,14 +8,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./reward.page.scss'],
 })
 export class RewardPage implements OnInit {
-
-  constructor(private router: Router) { }
+  chestImg: String;
+  constructor(private router: Router,
+              private toastCtrl: ToastController,) { }
 
   ngOnInit() {
+    this.chestImg = "https://static.turbosquid.com/Preview/2015/07/05__10_19_53/Rend_2.jpg192aa8af-86bb-4267-8c12-085327c2d78bZoom.jpg";
   }
 
   claim() {
-      this.router.navigate(['tabs', 'tab3']);
+      this.chestImg = "https://static.turbosquid.com/Preview/2015/07/05__10_19_53/Rend_1.jpg43286581-07d6-4ef3-ab57-342a64c12346Zoom.jpg";
+      let toast = this.toastCtrl.create({  
+        message: `CONGRATULATIONS YOU WON A FREE DAILY TICKET!`,  
+        icon: 'warning-circle',
+        position: 'top',
+        color: 'success',
+        buttons: [
+          {
+            text: 'NEXT',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel clicked');
+              this.router.navigate(['tabs', 'tab3']);
+            }
+          }]
+      }).then((x) => {x.present();});
   }
+
+  
 
 }
